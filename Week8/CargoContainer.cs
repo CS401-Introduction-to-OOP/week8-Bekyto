de0871ea1657444049e82907f1860534d28310b4
Bekyto <vladkapi2000@gmail.com>
2026-03-24 18:48:45 +0200

    Did second part

---
+++ Week8/CargoContainer.cs
@@ -0,0 +1,25 @@
+﻿namespace Week8;
+
+public class CargoContainer<T> where T: DeliveryItem
+{
+    private List<T> _items;
+
+    public CargoContainer()
+    {
+        _items = new List<T>();
+    }
+    public void AddItem(T item)
+    {
+        _items.Add(item);
+    }
+
+    public double GetTotalCost()
+    {
+        double totalCost = 0;
+        foreach (var t in _items)
+        {
+            totalCost += t.CalulateCost();
+        }
+        return totalCost;
+    }
+}
--- Week8/Program.cs
+++ Week8/Program.cs
@@ -7,8 +7,14 @@ namespace Week8
         {
             var lettr = new Letter("0", 0.1);
             lettr.PrintInfo();
+            Console.WriteLine(lettr.CalulateCost());
             var prcl = new Parcel("1", 2.2, "10x10x10");
             prcl.PrintInfo();
+            Console.WriteLine(prcl.CalulateCost());
+            var crgo = new CargoContainer<DeliveryItem>();
+            crgo.AddItem(lettr);
+            crgo.AddItem(prcl);
+            Console.WriteLine(crgo.GetTotalCost());
         }
     }
 }
